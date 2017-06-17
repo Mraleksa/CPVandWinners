c12521f111bb034447c001d649b8d615105feed9
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-17 21:57:31 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -24,6 +24,10 @@ var change = data.getJSON().data.changes[data.getJSON().data.changes.length-1].r
 if(change=="itemPriceVariation"){	
 	var lotIdContracts = data.getJSON().data.items[0].relatedLot;
 	var dateSigned = data.getJSON().data.dateSigned;
+	var description = data.getJSON().data.items[0].description;
+	var cpv = data.getJSON().data.items[0].classification.id;
+	var winner = data.getJSON().data.suppliers[0].name;
+	var winnerAdress = data.getJSON().data.suppliers[0].contactPoint.email;
 	var amount = data.getJSON().data.value.amount;	
 	client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+data.getJSON().data.tender_id})
 		.then(function (data) {
@@ -42,9 +46,16 @@ if(change=="itemPriceVariation"){
 	//console.log(startAmount)
 	}
 	db.serialize(function() {	
-	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,startAmount INT,amount INT,cpv TEXT)");
-	var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?)"); 	
-	statement.run(item.dateModified,dateSigned,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.numberOfBids,startAmount,amount,data.getJSON().data.items[0].classification.description);
+	db.run("CREATE TABLE IF NOT EXISTS data (key TEXT,cpv TEXT,name TEXT,contactPoint TEXT,contractID TEXT,startAmount INT,amount INT)");
+		
+	//db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,startAmount INT,amount INT,cpv TEXT)");
+	var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?)");
+	//var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?)"); 	
+	statement.run(description,cpv,data.getJSON().data.tenderID,winner,winnerAdress,startAmount,amount);
+	
+	//statement.run(item.dateModified,dateSigned,
+	//data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.numberOfBids,
+	//startAmount,amount,data.getJSON().data.items[0].classification.description);
 	//console.log(change);
 	statement.finalize();
 	});
