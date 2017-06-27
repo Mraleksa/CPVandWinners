2a30a9f7961a44a348b838ceb4e765df22e70c05
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-27 13:41:42 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -31,7 +31,7 @@ if(data.getJSON().data.status=="complete")	{
 	var q;
 	for (q = 1; q <= data.getJSON().data.contracts.length; q++) {
 		
-		description = data.getJSON().data.contracts[0].items.description.toLowerCase();
+		description = data.getJSON().data.contracts[q-1].items.description.toLowerCase();
 		//cpv = data.getJSON().data.contracts[0].items.classification.id;
 		//mail = data.getJSON().data.contracts[0].suppliers.contactPoint.email;
 		//edr = data.getJSON().data.contracts[0].suppliers.identifier.id;
