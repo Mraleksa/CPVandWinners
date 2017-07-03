07b01adc09388723c2f5c7f0104e3258a8c12a76
Mraleksa <a.gribanovskiy@gmail.com>
2017-07-03 11:30:44 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -38,7 +38,7 @@ if(data.getJSON().data.status=="complete")	{
 		mail = data.getJSON().data.contracts[q].suppliers[0].contactPoint.email;
 		edr = data.getJSON().data.contracts[q].suppliers[0].identifier.id;
 		winner = data.getJSON().data.contracts[q].suppliers[0].name;
-		region = data.getJSON().data.contracts[q].suppliers[0].address.region;
+		region = data.getJSON().data.contracts[q].suppliers[0].address.replace(/\s.+/, "");
 		amount = data.getJSON().data.contracts[q].value.amount;
 		
 		//console.log(cpv)
@@ -57,15 +57,15 @@ else {
 		amount = "";
 };
 
-				
+			
 					
 db.serialize(function() {
 db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,tenderID TEXT,status TEXT,name TEXT,description TEXT,cpv TEXT,mail TEXT,edr TEXT,winner TEXT,region TEXT)");
 var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?,?,?)");
-statement.run(dateModified.replace(/T.*/, ""),tenderID,status,name,description,cpv,mail,edr,winner,region.replace(/область|обл|обл./, ""));
+statement.run(dateModified.replace(/T.*/, ""),tenderID,status,name,description,cpv,mail,edr,winner,region);
 statement.finalize();
 });
-					//.replace(/\s.+/, "")
+					
 			
 					})
 					.catch(function  (error) {
